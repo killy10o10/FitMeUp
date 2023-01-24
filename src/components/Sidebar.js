b82fe6a18f82c1aeb7aa973a70fa7e4214d52338
muskan2532150 <rg2532150@gmail.com>
2023-01-24 21:51:21 +0530

    Fix eslint error

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,17 +5,24 @@ import {
 
 const Sidebars = () => {
   const { collapseSidebar, collapsed } = useProSidebar();
+
   return (
-    <div className={collapsed ?"d-flex" : "d-flex shadow h-100"} style={{ minHeight: '0px' }}>
+    <div className={`d-flex ${collapsed ? '' : ' shadow h-100'}`} style={{ minHeight: '0px' }}>
       <Sidebar className="sidebar" backgroundColor="transparent" defaultCollapsed transitionDuration={10} collapsedWidth="0px">
-        <Menu className='pt-5'>
+        <Menu className="pt-5">
           <MenuItem component={<Link to="/" />}> Trainer</MenuItem>
           <MenuItem component={<Link to="/" />}> User</MenuItem>
           <MenuItem component={<Link to="/" />}> Appoinment</MenuItem>
         </Menu>
       </Sidebar>
       <div className="position-relative">
-        <button type='button' onClick={() => collapseSidebar()} className={collapsed ? 'btn fs-3' : 'btn fs-3 position-absolute z-3 sidebar-btn'}><i className={collapsed ? 'bi bi-list' : 'bi bi-x-lg'} /></button>
+        <button
+          type="button"
+          onClick={() => collapseSidebar()}
+          className={`btn fs-3 ${collapsed ? '' : ' position-absolute z-3 sidebar-btn'}`}
+        >
+          <i className={`bi ${collapsed ? 'bi-list' : 'bi-x-lg'}`} />
+        </button>
       </div>
     </div>
   );

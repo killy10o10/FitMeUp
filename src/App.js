d89e7314268dac062a06b93005da3255bd3224d4
muskan2532150 <rg2532150@gmail.com>
2023-01-24 21:35:58 +0530

    Add Router and sidebar in 'App.js' file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import './assets/Main.css';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import Sidebars from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center">Fit Me UP</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Sidebars />
+        <Routes>
+          {/* <Route path="/" element={<Trainer />} />
+        <Route path="/details" element={<Popup />} />
+        <Route path="/appoiment" element={<Appoiment/>} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 

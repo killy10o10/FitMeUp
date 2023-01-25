cc088f47741d511e9c3ece460a37a60629bd168d
muskan2532150 <rg2532150@gmail.com>
2023-01-25 23:56:39 +0530

    Fix linter error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import './assets/Main.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
 import Sidebars from './components/Sidebar';
-import Datepickers from './components/Datepicker';
 
 function App() {
   return (
@@ -11,7 +10,7 @@ function App() {
         <Sidebars />
         <Routes>
           {/* <Route path="/" element={<Trainer />} /> */}
-          <Route path="/datepicker" element={<Datepickers />} />
+          {/* <Route path="/datepicker" element={<Datepickers />} /> */}
           {/* <Route path="/appoiment" element={<Appoiment/>} /> */}
         </Routes>
       </BrowserRouter>

da2f400ab9a6578b0a907393da3d312dbca21517
skyv26 <dev.aakashv@gmail.com>
2023-01-25 13:38:27 +0530

    Add the `dispatch-method` in `App.js`

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { FitMeUpThunk } from './redux/fitMeUpSlice';
 import Home from './components/Home';
 // import Appointment from './components/Appointment';
 
-function App() {
+const App = () => {
+  const trainerDispatch = useDispatch();
+
+  useEffect(() => {
+    trainerDispatch(FitMeUpThunk());
+  }, [trainerDispatch]);
   return (
     <div className="App">
       <Home />

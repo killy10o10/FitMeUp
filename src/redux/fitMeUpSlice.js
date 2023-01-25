e651bf31002f7806109bdb1225336000c2556aeb
skyv26 <dev.aakashv@gmail.com>
2023-01-25 13:28:18 +0530

    Implement the `slicer` for `FitMeUp`

---
+++ src/redux/fitMeUpSlice.js
@@ -0,0 +1,28 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const TrainerApiUrl = '/api/v1/trainer/trainer.json';
+
+const TrainerRequestMethod = '/get/trainers';
+
+const initialState = [];
+
+const FitMeUpThunk = createAsyncThunk(TrainerRequestMethod, async () => {
+  const trainerData = await (await (fetch(TrainerApiUrl))).json();
+  return trainerData;
+});
+
+const FitMeUpSlice = createSlice({
+  name: 'FitMeUp',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(FitMeUpThunk.fulfilled, (state, action) => {
+      const { payload } = action;
+      return {
+        ...payload,
+      };
+    }),
+  },
+});
+
+export default FitMeUpSlice.reducer;

2507b74d88d4d2475b63f6ea68f7f9acd5a318a7
muskan2532150 <rg2532150@gmail.com>
2023-01-26 18:37:01 +0530

    Fix dateform and remove linter error

--- src/components/Datepicker.js
+++ src/components/Datepicker.js
@@ -3,12 +3,16 @@ import React from 'react';
 const Datepickers = () => (
   <main className="d-flex justify-content-center align-items-center date-form">
     <form action="/">
-      <label htmlFor="Start-date">
-        Date:
-        <br />
-        <input type="date" name="date" id="Start-date" />
-      </label>
-      <input type="submit" />
+      <div className="mb-3">
+        <label htmlFor="Start-date" className="form-label">
+          {' '}
+          Date:
+          <input type="date" name="date" id="Start-date" className="form-control" />
+        </label>
+      </div>
+      <div className="mb-3">
+        <input type="submit" className="btn btn-outline-success text-white light date-button" />
+      </div>
     </form>
   </main>
 );

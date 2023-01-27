6d51d1fbb1d39773c406a52c8025f3fd864e8cf2
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-01-27 02:38:48 +0000

    Add Book trainer button on trainer details page
    Style trianer details page

--- src/components/TrainerDetails.js
+++ src/components/TrainerDetails.js
@@ -4,18 +4,18 @@ import { useLocation } from 'react-router-dom';
 const TrainerDetails = () => {
   const location = useLocation();
   const { state } = location;
-
+  const trainerSpecialty = state.speciality.map((item) => item).join(', ');
   return (
-    <section className="trainer-details d-flex">
+    <section className="trainer-details h-100 align-items-center d-flex">
       <div className="trainer-img d-flex justify-content-center">
         <img src={state.profile_pic} alt={state.full_name} />
       </div>
       <div className="trainer-details d-flex flex-column">
         <div className="trainer-name">
           <h1>{state.full_name}</h1>
-          {state.speciality.map((item) => <small key={item}>{item}</small>)}
+          <small className="fst-italic">{trainerSpecialty}</small>
         </div>
-        <table className="table table-striped">
+        <table className="table table-striped shadow-sm">
           <tbody>
             <tr>
               <td>Location</td>
@@ -33,12 +33,12 @@ const TrainerDetails = () => {
               <td>Weight</td>
               <td>{state.weight_in_kg}</td>
             </tr>
-            <tr>
-              <td>Weight</td>
-              <td>{state.weight_in_kg}</td>
-            </tr>
           </tbody>
         </table>
+        <div className="booking shadow-lg d-flex align-items-center p-1  mt-4">
+          <i className="bi bi-bookmark-check mx-1 fs-4 text-white" />
+          <button className="book-btn book mx-4" type="button">Book Trainer</button>
+        </div>
       </div>
     </section>
   );

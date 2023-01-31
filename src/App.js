14c5fa9cc957a56c0e5b533b6048430f11338347
muskan2532150 <rg2532150@gmail.com>
2023-01-31 13:16:24 +0530

    Add SpecilaityThunk in App file

--- src/App.js
+++ src/App.js
@@ -6,19 +6,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Sidebars from './components/Sidebar';
 import { FitMeUpThunk } from './redux/fitMeUpSlice';
+import { SpecilatiyThunk } from './redux/specilatiySlice';
 import Home from './components/Home';
 import Appointment from './components/Appointment';
 import Datepickers from './components/Datepicker';
 import TrainerDetails from './components/TrainerDetails';
 import SearchLocation from './components/SearchLocation';
+import Specilatiy from './components/Specilatiy';
 
 const App = () => {
   const trainersArray = useSelector((state) => state.trainers);
+  const specilatiyArray = useSelector((state) => state.specilatiys);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (trainersArray.length === 0) {
       dispatch(FitMeUpThunk());
+    }
+    if (specilatiyArray.length === 0) {
+      dispatch(SpecilatiyThunk());
     }
   }, []);
 
@@ -32,6 +39,7 @@ const App = () => {
           <Route path="/appointment" element={<Appointment trainers={trainersArray} />} />
           <Route path="/trainerDetails" element={<TrainerDetails />} />
           <Route path="/search" element={<SearchLocation />} />
+          <Route path="/specilatiy" element={<Specilatiy specilatiy={specilatiyArray} />} />
         </Routes>
       </BrowserRouter>
     </div>

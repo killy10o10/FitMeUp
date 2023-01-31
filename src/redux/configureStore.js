3dd63e6721142df5094083f4c05df9b6f110f1f5
muskan2532150 <rg2532150@gmail.com>
2023-01-31 13:15:17 +0530

    Add SpecilatiySliceReducer in store file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import fitMeUpSlice from './fitMeUpSlice';
+import SpecilatiySlice from './specilatiySlice';
 
 const reducer = combineReducers({
   trainers: fitMeUpSlice,
+  specilatiys: SpecilatiySlice,
 });
 
 const store = configureStore({

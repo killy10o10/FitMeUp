6adc8fcd8b0df7de888f19a116a230176fb4dd07
muskan2532150 <rg2532150@gmail.com>
2023-01-31 16:15:58 +0530

    Update Form for user

--- src/components/Trainer.js
+++ src/components/Trainer.js
@@ -1,45 +1,46 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 
-const Trainer = ({ trainers }) => (
-  <div className="container pt-5">
-    <form>
-      <div className="mb-3">
-        <label htmlFor="fullName" className="form-label">Full Name</label>
-        <input type="text" className="form-control" id="fullName" />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="address" className="form-label">Address</label>
-        <input type="text" className="form-control" id="fullName" />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="phoneno" className="form-label">Phone Number</label>
-        <input type="number" className="form-control" id="phoneno" />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="height" className="form-label"> Height</label>
-        <input type="number" className="form-control" id="height" />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="weight" className="form-label">Weight</label>
-        <input type="number" className="form-control" id="weight" />
-      </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
-    <p className='text-center p-3 fs-3'> Trainer List</p>
-    <div className="border p-2 mt-3">
-      {trainers.map((el, index) => (
-        <div key={index} className="d-flex justify-content-between mb-3 border-bottom pb-2">
-          <p>{el.full_name}</p>
-          <button className="btn btn-danger" type="button">Delete</button>
+const Trainer = ({ trainers, users }) => {
+  const [selectedUser, setselectedUser] = useState('');
+  const handleChange = (e) => {
+    setselectedUser(e.target.value);
+  };
+  return (
+    <div className="container pt-5">
+      <p className="text-center fs-4 p-3"> Trainer Form</p>
+      <form>
+        <select
+          className="form-select mb-3"
+          size="lg"
+          name="users"
+          id="users"
+          value={selectedUser}
+          onChange={handleChange}
+        >
+          {users.map((el) => <option key={el.id} value={el.id}>{el.full_name}</option>)}
+        </select>
+        <div className="mb-3">
+          <label htmlFor="price" className="form-label">Price</label>
+          <input type="number" className="form-control" id="price" />
         </div>
-      ))}
+        <div className="mb-3">
+          <label htmlFor="bio" className="form-label">Bio</label>
+          <input type="text" className="form-control" id="bio" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+      <p className="text-center p-3 fs-3"> Trainer List</p>
+      <div className="border p-2 mt-3">
+        {trainers.map((el, index) => (
+          <div key={index} className="d-flex justify-content-between mb-3 border-bottom pb-2">
+            <p>{el.full_name}</p>
+            <button className="btn btn-danger" type="button">Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Trainer;

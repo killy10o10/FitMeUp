e725c5b011efc452fe8f7b7b2ea4c2a5f9638ebd
muskan2532150 <rg2532150@gmail.com>
2023-01-31 16:12:45 +0530

    Import userSlice in store file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import fitMeUpSlice from './fitMeUpSlice';
 import SpecilatiySlice from './specilatiySlice';
+import userSlice from './userSlice';
 
 const reducer = combineReducers({
   trainers: fitMeUpSlice,
   specilatiys: SpecilatiySlice,
+  users: userSlice,
 });
 
 const store = configureStore({

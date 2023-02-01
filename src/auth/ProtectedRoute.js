e08cc422ec7f957c32400b015515a6e09b079d4f
muskan2532150 <rg2532150@gmail.com>
2023-02-01 13:32:36 +0530

    Pass previous location path to login page

--- src/auth/ProtectedRoute.js
+++ src/auth/ProtectedRoute.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from './Provider';
 
 const ProtectedRoute = ({ children }) => {
+  const location = useLocation();
   const { token } = useAuth();
   if (!token) {
-    return <>
-    <Navigate to="/login" replace />;
-    </>
+    return (
+      <>
+        <Navigate to="/login" replace state={{ from: location }} />
+        ;
+      </>
+    );
   }
 
   return children;

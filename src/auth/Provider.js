e43775abe2096b41e9057dadf9a99d8f74dea554
muskan2532150 <rg2532150@gmail.com>
2023-02-01 12:41:26 +0530

    Create custom hooks for context provider and useContext for provider value

---
+++ src/auth/Provider.js
@@ -0,0 +1,36 @@
+import React, { createContext, useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import fakeAuth from './Auth';
+
+const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null);
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    const token = await fakeAuth();
+    setToken(token);
+    navigate('/');
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+  };
+
+  const value = {
+    token,
+    onLogin: handleLogin,
+    onLogout: handleLogout,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };

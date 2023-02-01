8f2b0f7dfdb19cf58dee2b8b0b1e4efab8973f1b
muskan2532150 <rg2532150@gmail.com>
2023-02-01 20:54:56 +0530

    conditional for guest and admin

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,19 +4,24 @@ import {
   Sidebar, Menu, MenuItem, useProSidebar,
 } from 'react-pro-sidebar';
 import { useAuth } from '../auth/Provider';
+import { useSelector } from 'react-redux';
+
 
 const Sidebars = () => {
   const { collapseSidebar, collapsed } = useProSidebar();
   const { token, onLogout } = useAuth();
+  const userArray = useSelector((state) => state.users);
 
   return (
     <div className={`d-flex z-1 h-100 position-absolute ${collapsed ? '' : ' shadow'}`}>
       <Sidebar className="sidebar" backgroundColor="#96bf02c9" defaultCollapsed transitionDuration={100} collapsedWidth="0px">
         <Menu className="pt-5">
           <MenuItem component={<Link to="/" />} onClick={() => collapseSidebar()}> Home</MenuItem>
-          <MenuItem component={<Link to="/appointment" />} onClick={() => collapseSidebar()}> Appoinment</MenuItem>
-          <MenuItem component={<Link to="/specilatiy" />} onClick={() => collapseSidebar()}> Specilatiy</MenuItem>
-          <MenuItem component={<Link to="/trainer" />} onClick={() => collapseSidebar()}> Trainer</MenuItem>
+          {userArray.length===0  ? '' :(<>
+            <MenuItem component={<Link to="/appointment" />} onClick={() => collapseSidebar()}> Appoinment</MenuItem>
+            <MenuItem component={<Link to="/specilatiy" />} onClick={() => collapseSidebar()}> Specilatiy</MenuItem>
+            <MenuItem component={<Link to="/trainer" />} onClick={() => collapseSidebar()}> Trainer</MenuItem>
+          </>) }
           <MenuItem
             component={token ? <Link to="/" onClick={onLogout} />
               : <Link to="/login" />}

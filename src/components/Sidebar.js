1d3b0fd5f5bce1fa97af506d5a776969210b0d31
John <kibirigejohn005@gmail.com>
2023-02-01 15:54:18 +0300

    refactor collapseSidebar logic

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,37 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-  Sidebar, Menu, MenuItem, useProSidebar,
-} from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar';
 import { useAuth } from '../auth/Provider';
 
 const Sidebars = () => {
   const { collapseSidebar, collapsed } = useProSidebar();
   const { token, onLogout } = useAuth();
+  const handleClick = () => {
+    collapseSidebar();
+  };
 
   return (
-    <div className={`d-flex z-1 h-100 position-absolute ${collapsed ? '' : ' shadow'}`}>
-      <Sidebar className="sidebar" backgroundColor="#96bf02c9" defaultCollapsed transitionDuration={100} collapsedWidth="0px">
-        <Menu className="pt-5">
-          <MenuItem component={<Link to="/" />} onClick={() => collapseSidebar()}> Home</MenuItem>
-          <MenuItem component={<Link to="/appointment" />} onClick={() => collapseSidebar()}> Appoinment</MenuItem>
-          <MenuItem component={<Link to="/specilatiy" />} onClick={() => collapseSidebar()}> Specilatiy</MenuItem>
-          <MenuItem component={<Link to="/trainer" />} onClick={() => collapseSidebar()}> Trainer</MenuItem>
+    <div
+      className={`d-flex z-1 h-100 position-absolute ${
+        collapsed ? '' : ' shadow'
+      }`}>
+      <Sidebar
+        className='sidebar'
+        backgroundColor='#96bf02c9'
+        defaultCollapsed
+        transitionDuration={100}
+        collapsedWidth='0px'>
+        <Menu className='pt-5'>
+          <MenuItem component={<Link to='/' />} onClick={handleClick}>
+            {' '}
+            Home
+          </MenuItem>
           <MenuItem
-            component={token ? <Link to="/" onClick={onLogout} />
-              : <Link to="/login" />}
-            onClick={() => collapseSidebar()}
-          >
+            component={<Link to='/appointment' />}
+            onClick={handleClick}>
+            {' '}
+            Appoinment
+          </MenuItem>
+          <MenuItem component={<Link to='/specilatiy' />} onClick={handleClick}>
+            {' '}
+            Specilatiy
+          </MenuItem>
+          <MenuItem component={<Link to='/trainer' />} onClick={handleClick}>
+            {' '}
+            Trainer
+          </MenuItem>
+          <MenuItem
+            component={
+              token ? <Link to='/' onClick={onLogout} /> : <Link to='/login' />
+            }
+            onClick={handleClick}>
             {token ? 'Sign Out' : 'Sign In'}
           </MenuItem>
         </Menu>
       </Sidebar>
-      <div className="position-relative z-3">
+      <div className='position-relative z-3'>
         <button
-          type="button"
-          onClick={() => collapseSidebar()}
-          className={`btn hamburger fs-3 ${collapsed ? '' : ' position-absolute sidebar-btn'}`}
-        >
+          type='button'
+          onClick={handleClick}
+          className={`btn hamburger fs-3 ${
+            collapsed ? '' : ' position-absolute sidebar-btn'
+          }`}>
           <i className={`bi ${collapsed ? 'bi-list' : 'bi-x-lg'}`} />
         </button>
       </div>

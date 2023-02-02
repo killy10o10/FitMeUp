c8cc80d8b6e8d8863632965e83208d5ebd86dd1b
muskan2532150 <rg2532150@gmail.com>
2023-02-02 17:58:50 +0530

    Add login,signup, user info fetch function

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,5 +1,51 @@
-const fakeAuth = () => new Promise((resolve) => {
-  setTimeout(() => resolve('2342f2f1d131rf12'), 250);
-});
+import { useSelector } from 'react-redux';
 
-export default fakeAuth;
+export const signupUser = async (data) => (
+  await (await fetch('http://127.0.0.1:3001/api/v1/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(data),
+  })).json()
+);
+
+export const gettoken = async (item) => (
+  await (await fetch('http://127.0.0.1:3001/api/v1/secure/auth_login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(item),
+  })).json()
+);
+
+export const loginUser = async (token) => (await (await fetch('http://127.0.0.1:3001/api/v1/users', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: token,
+  },
+})).json());
+
+// export const logoutUser = () => (dispatch) => fetch('http://localhost:3000/logout', {
+//   method: 'DELETE',
+//   headers: {
+//     Accept: 'application/json',
+//     'Content-Type': 'application/json',
+//     Authorization: getToken(),
+//   },
+// }).then((res) => {
+//   deleteToken();
+//   if (res.ok) {
+//     return res.json()
+//       .then(() => dispatch({ type: NOT_AUTHENTICATED }));
+//   }
+//   return res.json().then((errors) => {
+//     dispatch({ type: NOT_AUTHENTICATED });
+//     return Promise.reject(errors);
+//   });
+// });

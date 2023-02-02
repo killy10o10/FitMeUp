289319bd97b282d405025211d15222f5cda41656
John <kibirigejohn005@gmail.com>
2023-02-02 11:53:46 +0300

    change the importation logic of all react icons

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,48 +1,39 @@
 import React from 'react';
-import {
-  FaLinkedinIn,
-  BsTwitter,
-  TfiGoogle,
-  FaFacebookF,
-  FaPinterestP,
-} from 'react-icons/all';
+import { FaLinkedinIn, FaFacebookF, FaPinterestP } from 'react-icons/fa';
+import { BsTwitter } from 'react-icons/bs';
+import { TfiGoogle } from 'react-icons/tfi';
 
 const Socials = () => (
-  <footer className="socials">
-    <div className="socials-container">
+  <footer className='socials'>
+    <div className='socials-container'>
       <a
-        href="https://github.com/killy10o10/FitMeUp.git"
-        className="socials-link"
-      >
-        <BsTwitter className="socials-icon" />
+        href='https://github.com/killy10o10/FitMeUp.git'
+        className='socials-link'>
+        <BsTwitter className='socials-icon' />
       </a>
       <a
-        href="https://github.com/killy10o10/FitMeUp.git"
-        className="socials-link"
-      >
-        <FaFacebookF className="socials-icon" />
+        href='https://github.com/killy10o10/FitMeUp.git'
+        className='socials-link'>
+        <FaFacebookF className='socials-icon' />
       </a>
       <a
-        href="https://github.com/killy10o10/FitMeUp.git"
-        className="socials-link"
-      >
-        <TfiGoogle className="socials-icon" />
+        href='https://github.com/killy10o10/FitMeUp.git'
+        className='socials-link'>
+        <TfiGoogle className='socials-icon' />
       </a>
       <a
-        href="https://github.com/killy10o10/FitMeUp.git"
-        className="socials-link"
-      >
-        <FaLinkedinIn className="socials-icon" />
+        href='https://github.com/killy10o10/FitMeUp.git'
+        className='socials-link'>
+        <FaLinkedinIn className='socials-icon' />
       </a>
       <a
-        href="https://github.com/killy10o10/FitMeUp.git"
-        className="socials-link"
-      >
-        <FaPinterestP className="socials-icon" />
+        href='https://github.com/killy10o10/FitMeUp.git'
+        className='socials-link'>
+        <FaPinterestP className='socials-icon' />
       </a>
     </div>
 
-    <h2 className="socials-copyright">&#169; 2023 FitMeUp - damkj</h2>
+    <h2 className='socials-copyright'>&#169; 2023 FitMeUp - damkj</h2>
   </footer>
 );
 

7179d57fdfcef355779619b9dc9eebd0db4cea4b
muskan2532150 <rg2532150@gmail.com>
2023-02-02 18:05:52 +0530

    Remove UserSliceReducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import fitMeUpSlice from './fitMeUpSlice';
 import SpecilatiySlice from './specilatiySlice';
-import userSlice from './userSlice';
 import UserSliceReducer from './tokenSlice';
 
 const reducer = combineReducers({
   trainers: fitMeUpSlice,
   specilatiys: SpecilatiySlice,
-  users: userSlice,
   currentuser: UserSliceReducer,
 });
 

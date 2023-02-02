30f1e573c092deb126d4feb5f81dc8c350c0314a
muskan2532150 <rg2532150@gmail.com>
2023-02-02 17:13:56 +0530

    Add token Slice Reducer file in store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,13 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import fitMeUpSlice from './fitMeUpSlice';
 import SpecilatiySlice from './specilatiySlice';
 import userSlice from './userSlice';
+import UserSlice from './tokenSlice';
 
 const reducer = combineReducers({
   trainers: fitMeUpSlice,
   specilatiys: SpecilatiySlice,
   users: userSlice,
+  currentuser: UserSlice,
 });
 
 const store = configureStore({

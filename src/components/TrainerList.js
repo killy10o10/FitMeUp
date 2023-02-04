ef17ccda0b4f2c64cfd09342495eb33a0cfd94b8
skyv26 <dev.aakashv@gmail.com>
2023-02-04 17:20:53 +0530

    Remove the `commented` code

--- src/components/TrainerList.js
+++ src/components/TrainerList.js
@@ -16,7 +16,6 @@ const breakPoints = [
 const TrainerList = (props) => {
   const { trainers } = props;
   const trainerCardBackgroundColors = getBackgroundColor(trainers.length);
-  const currentUser = useSelector((state) => state.currentuser);
   const trainerCard = trainers.map((item, index) => {
     const { full_name, profile_pic, bio } = item;
     const limitedBio = bio ? bio.split(' ').slice(0, 15).join(' ') : '';

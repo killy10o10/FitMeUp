59f4a38f7559620744a021e606274ed832cd92e8
skyv26 <dev.aakashv@gmail.com>
2023-02-04 17:11:13 +0530

    Fix the issue

--- src/components/Speciality.js
+++ src/components/Speciality.js
@@ -3,13 +3,14 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { specialityForm } from '../auth/Auth';
 
-const Speciality = ({ speciality }) => {
+const Speciality = () => {
+  const dispatch = useDispatch();
   const [state, setState] = useState({
     name: '',
   });
 
-  const dispatch = useDispatch();
-  const token = useSelector((state) => state.currentuser.token);
+  const data = useSelector((state) => state.currentuser);
+  console.log('Killy Bro....', data);
 
   const handleChange = (e) => {
     setState((prevState) => ({
@@ -20,7 +21,7 @@ const Speciality = ({ speciality }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    specialityForm(state, token, dispatch);
+    specialityForm(state, data.token, dispatch);
   };
 
   return (
@@ -35,7 +36,7 @@ const Speciality = ({ speciality }) => {
       </form>
       <p className="text-center fs-3 p-2">Speciality List</p>
       <div className="border p-2 mt-3">
-        {speciality.map((el) => (
+        {data.specialities.map((el) => (
           <div key={el.id} className="d-flex justify-content-between mb-3 border-bottom pb-2">
             <p>{el.name}</p>
             <button className="btn btn-danger" type="button">Delete</button>

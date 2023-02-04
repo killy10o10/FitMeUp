fb5d9f47abf959249fde0b99e0f40a110b4c0ce8
skyv26 <dev.aakashv@gmail.com>
2023-02-04 17:12:41 +0530

    Improve the functionality

--- src/redux/tokenSlice.js
+++ src/redux/tokenSlice.js
@@ -1,44 +1,103 @@
 /* eslint no-param-reassign: "error" */
-import { createSlice } from '@reduxjs/toolkit';
-import { SpecialityThunk } from './specilatiySlice';
-import { gettoken, loginUser } from '../auth/Auth';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// import { SpecialityThunk } from './specilatiySlice';
+// import { loginUser } from '../auth/Auth';
+
+const SpecialityApiUrl = 'http://127.0.0.1:3001/api/v1/specialities';
+
+export const getUser = createAsyncThunk('/jcjdc/adahgshs', async (item) => {
+  const result = await (await fetch('http://127.0.0.1:3001/api/v1/secure/auth_login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(item),
+  })).json();
+
+  if (result.token !== undefined) {
+    const user = (await (await fetch('http://127.0.0.1:3001/api/v1/users', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: result.token,
+      },
+    })).json());
+    let obj = {};
+    if (user.role === 'admin') {
+      const specialities = await (await (fetch(SpecialityApiUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: result.token,
+        },
+      }))).json();
+      obj = {
+        token: result.token,
+        data: user,
+        specialities,
+      };
+    } else {
+      obj = {
+        token: result.token,
+        data: user,
+      };
+    }
+    return obj;
+  }
+  const { token } = result;
+  return {
+    token,
+  };
+});
 
 export const UserSlice = createSlice({
   name: 'user',
   initialState: {
     token: '',
-    data: [],
+    data: {},
+    specialities: [],
   },
   reducers: {
     getdata: (state, action) => { state.data = action.payload; },
-    settoken: (state, action) => { state.token = action.payload; },
+    // settoken: (state, action) => { state.token = action.payload; },
     // appendappointment: (state, action) => {
     //   return {...state,
     //     state.data.appointments: [...state.data.appointments,action.payload]
     //   }
     // },
+    signout: () => ({ token: '', data: {} }),
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getUser.fulfilled, (state, action) => {
+      const { payload } = action;
+      return payload;
+    });
   },
 });
 
 export const {
-  getdata, userChange, traineradd, settoken,
+  getdata, signout,
 } = UserSlice.actions;
 
 export default UserSlice.reducer;
 
-export function fetchdata(item) {
-  return async function fetchdataThunk(dispatch) {
-    try {
-      const { token } = await gettoken(item);
-      dispatch(settoken(token));
-      if (token !== 'unauthorized') {
-        const data = await loginUser(token);
-        dispatch(getdata(data));
-        if (data.role === 'admin') dispatch(SpecialityThunk(token));
-      }
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.warn('Unauthorized');
-    }
-  };
-}
+// export function fetchdata(item) {
+//   return async function fetchdataThunk(dispatch) {
+//       };
+// }
+
+// export const fetchUser = createAsyncThunk('/ashasghdsgd', async ({ dispatch, item }) => {
+//   try {
+//     if (token !== 'unauthorized') {
+//       const data = await loginUser(token);
+//       dispatch(getdata(data));
+//       if (data.role === 'admin') dispatch(SpecialityThunk(token));
+//     }
+//   } catch (error) {
+//     // eslint-disable-next-line no-console
+//     console.warn('Unauthorized');
+//   }
+// });

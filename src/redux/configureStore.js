b79c01a58d46cd1258e549b2b2ea358ccf9323de
skyv26 <dev.aakashv@gmail.com>
2023-02-04 18:55:20 +0530

    Remove the commented line

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,9 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import fitMeUpSlice from './fitMeUpSlice';
-import SpecialitySlice from './specilatiySlice';
 import UserSliceReducer from './tokenSlice';
 
 const reducer = combineReducers({
   trainers: fitMeUpSlice,
-  specialities: SpecialitySlice,
   currentuser: UserSliceReducer,
 });
 
